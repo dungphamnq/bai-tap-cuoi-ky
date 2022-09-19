423232e641ebe9a83c7d83be21c5717927cea608
dungphamnq <dungphamnq@gmail.com>
2022-09-19 16:06:06 +0700

    .add

--- anime.js
+++ anime.js
@@ -1,3 +1,11 @@
 function topFunction() {
   document.documentElement.scrollTop = 0;
 }
+
+window.addEventListener("load", function () {
+  document
+    .querySelector(".email-contact")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+    });
+});
